9bad15f11d794f2adbf8865bb468aad5f2bdf116
Ranjan Kumar <67799181+ranjan-alt@users.noreply.github.com>
2025-02-28 13:58:58 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/",(req,res)=>{
+    res.send("Api is running")
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
